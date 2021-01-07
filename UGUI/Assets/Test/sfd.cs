60a22badbb831bf1f20c0a01339bf0c861040242
seven <zhl@gameduchy.com>
2021-01-07 21:54:52 +0800

    提交

---
+++ UGUI/Assets/Test/sfd.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class sfd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DebugAllPosition();
+        PositionAndLocalPosition();
+    }
+
+    void DebugAllPosition()
+    {
+        RectTransform rect = GetComponent<RectTransform>(); 
+        Debug.Log("position:\t\t" + transform.position);
+        Debug.Log("localPosition:\t\t" + transform.localPosition);
+        Debug.Log("anchoredPosition:\t\t" + rect.anchoredPosition);
+        Debug.Log("anchoredPosition3D:\t\t" + rect.anchoredPosition3D);
+    }
+    
+    void PositionAndLocalPosition()
+    {
+        Debug.Log("position:\t\t" + transform.position);
+
+        Transform t = transform;
+        Vector3 pos = Vector3.zero;
+        while(t != null)
+        {
+            pos += t.localPosition;
+            t = t.parent;
+        }
+        Debug.Log("localPositon sum:\t" + pos);
+    }
+}
---
+++ UGUI/Assets/testddd.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class testddd : MonoBehaviour, IPointerClickHandler
+{
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.LogError(eventData);
+        // StartCoroutine("LongPress")
+    }
+
+    void OnMouseDown()
+    {
+        Debug.LogError("OnMouseDown");
+    }
+
+    void OnMouseUpAsButton()
+    {
+        Debug.LogError("OnMouseUpAsButton");
+    }
+
+    void OnMouseUp()
+    {
+        Debug.LogError("OnMouseUp");
+    }
+
+    void OnMouseDrag()
+    {
+        Debug.LogError("OnMouseDrag");
+    }
+
+    void OnMouseOver()
+    {
+        Debug.LogError("OnMouseOver");
+    }
+
+    void OnMouseExit()
+    {
+        Debug.LogError("OnMouseExit");
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.LogError("OnMouseEnter");
+    }
+    
+}
